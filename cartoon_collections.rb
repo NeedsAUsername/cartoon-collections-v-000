f0d16f95663ffc8d53d5b4fb7d5ef7dbf68462ce
Kevin Chi <kevinchi118@gmail.com>
2018-06-13 23:42:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ end
 
 def summon_captain_planet(array)
     array.each do |element|
-        element = element.charAt(0).toUpperCase
+        element = "#{element.capitalize}!"
+    end 
 end
 
 def long_planeteer_calls# code an argument here
